ec4f2bbee3089e85be6c8cb62beadb1276d3148a
Tu Nguyen <nvminhtu@gmail.com>
2016-07-19 14:43:31 +0700

    es6  tutorial source

---
+++ es6/projects/es6-tutorial/build/main.bundle.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+    var monthlyRate = 0;
+    if (rate) {
+        monthlyRate = rate / 100 / 12;
+    }
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
+    return {
+        principal: principal,
+        rate: rate,
+        monthlyPayment: monthlyPayment,
+        monthlyRate: monthlyRate
+    };
+};
+
+document.getElementById('calcBtn').addEventListener('click', function () {
+    var principal = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+    var rate = document.getElementById("rate").value;
+
+    var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate);
+
+    var monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
+    var monthlyRate = _calculateMonthlyPaym.monthlyRate;
+
+    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
+});
